02a1e01e9b3a2cedf2c4b2bc0a45f3a89f835798
summer <wpsumsun@126.com>
2018-03-04 22:36:13 +0800

    返回顶部

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -5,6 +5,7 @@ import url from 'js/api.js'
 import './search.css'
 import 'css/common.css'
 import mixin from 'js/mixin.js'
+import Velocity from 'velocity-animate'
 
 let { keyword, id } = qs.parse(location.search.substr(1))
 
@@ -13,6 +14,7 @@ new Vue({
   data: {
     searchLists: null,
     keyword,
+    isShowTop: false,
   },
   mixins: [mixin],
   created() {
@@ -23,6 +25,16 @@ new Vue({
       axios.post(url.searchlist, { keyword, id }).then(res => {
         this.searchLists = res.data.lists
       })
+    },
+    move() {
+      if (document.body.scrollTop > 100) {
+        this.isShowTop = true
+        return
+      }
+      this.isShowTop = true
+    },
+    goTop() {
+      Velocity(document.body, 'scroll', { dulation: 400 })
     },
   },
 })
